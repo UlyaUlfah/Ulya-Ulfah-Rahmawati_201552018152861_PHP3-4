c166029b3c15b88b451641f5222013446339d361
Ulya Ulfah <114070867+UlyaUlfahNgoding@users.noreply.github.com>
2022-10-31 06:03:37 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Belajar PHP</title>
+</head>
+<body>
+
+  <?php
+/*
+  berapa baris
+  berapa komentar
+  terserah teman2
+*/
+
+  // variabel pada php
+  // tipe data -string-
+  $nama  = "Sekolah koding 123";
+  $nama2 = 'bermain koding';
+
+  echo "selamat datang di $nama <br>";
+
+  //tipe data angka/number
+  // 1, integer 2, float
+
+  $angka  = 1000;
+  $angka2 = 100.123;
+
+  echo $angka + $angka2;
+?>
+
+</body>
+</html>
